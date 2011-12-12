183c88ad3c4f56fa07606ffa12c229837164b206
Johannes Lindhart Borresen <johannes@the.homestead.dk>
2011-12-12 01:13:45 +0100

    Enabled todonotes package in the preamble.
    Added MySQL, JSON and AJAX to the list of tools/concepts.
    
    Added an alternative view section - maybe both should be used?
    
    Finished second revision of the controller section. I'm not satisfied, but I need some feedback on what to put there.

---
+++ tex/implementation/controller_example_contactbook.php
@@ -0,0 +1,131 @@
+/**
+ * Sub-controller for the Contactbook application. By definition cannot
+ * be called without an accompanying action *and* at least one
+ * parameter.
+ **/
+class Contactbook extends GirafController
+{
+	public function index()
+	{
+		die("Contactbook module cannot be called directly!");
+	}
+	
+	/**
+	 * We allow fallback to perform a default-type action with the
+	 * action parameter if it is a numeric character. In this case we
+	 * request a default action from the subcontroller tied to a child
+	 * id.
+	 **/
+	public function fallback($action, $params = array())
+	{
+		if (is_numeric($action)) $this->_default($action, $params);
+		elseif (strtolower($action) == "list") $this->_list($params);
+		else parent::fallback($action, $params);
+	}
+	
+	/**
+	 * This function is called by fallback if the action was given as
+	 * an integer. In this case, it is assumed that the calling client
+	 * requests the results of an index action tied to a specific child.
+	 **/
+	public function _default($action, $params = array())
+	{
+		// echo "Echoing default data for $action.";
+		// The default action is _list :D
+		
+		// We fake-construct a new path for the action to use.
+		$path = "/contactbook/list/$action";
+		$this->_list(GetAssocPath($path));
+	}
+	
+	public function show($params = array())
+	{
+		$data = array();
+		
+		if (!isset($s)) $s = GirafSession::getSession();
+
+		$userId = $s->getCurrentUser();
+		$data["userId"] = $userId;
+
+		if ($userId == null || $userId == false)
+		{
+			die("Page cannot be used without logging in.");
+		}
+
+		$userData = GirafUser::getGirafUser($userId);
+		$data["userData"] = $userData;
+
+		// By definition the parameter right after the action is the
+		// message id. That index is param0.
+		if (!isset($params["param0"])) die("Ingen besked ID efterspurgt!");
+		else $msgId = $params["param0"];
+
+		// Get message data.
+		$messageData = ContactbookMessage::getMessage($msgId);
+		// Mark it read. We loaded it, yeh?
+		$messageData->setRead($userId);
+		
+		$data["message"] = $messageData;
+		// Image data.
+		$data["images"] = $messageData->getImages();
+
+		// Get OP data.
+		$data["poster"] = GirafUser::getGirafUser($messageData->msgUserKey);
+
+		// Get current user.
+		if (!isset($s)) $s = GirafSession::getSession();
+		$userId = $s->getCurrentUser();
+
+		// Get replies.
+		$data["replies"] = $messageData->getReplies();
+		
+		$this->view("apps/contactbook/show", $data);
+	}
+	
+	/**
+	 * "list" is a reserved word in PHP, so we shadow the name
+	 * and call the function through the fallback function instead.
+	 **/
+	public function _list($params = array())
+	{
+		$data = array();
+		
+		if (!isset($s)) $s = GirafSession::getSession();
+
+		$userId = $s->getCurrentUser();
+		$userData = GirafUser::getGirafUser($userId);
+		$data["childId"] = $params["param0"];
+		$cond = 'msgChildKey=' . $data["childId"] . ' AND msgParentKey IS NULL';
+		$data["messages"] = ContactbookMessage::getMessages($cond, null, ContactbookMessage::RETURN_RECORD);
+		$data["username"] = $userData->username;
+		$data["userId"] = $userId;		
+		$this->view("apps/contactbook/list.php", $data);
+	}
+	
+	/**
+	 * Creates a new contactbook message.
+	 * */
+	public function add($params = array())
+	{
+		if (!isset($_POST["user"], $_POST["child"], $_POST["subject"], $_POST["body"]))
+		{
+			echo "failure";
+			return;
+		}
+		$msgId = ContactbookMessage::createNewMessage(	$_POST["user"],
+														$_POST["child"],
+														$_POST["subject"],
+														$_POST["body"],
+														isset($_POST["parent"]) ? $_POST["parent"] : null);
+		// Handle uploaded images. This is handled after message creation because we did not know the ID
+		// beforehand.
+		foreach ($_FILES as $file)
+		{
+			$path = BASEDIR . "img/" . $file["name"];
+			$res = move_uploaded_file($file["tmp_name"], $path);
+			// Save to database.
+			GirafImage::createMessageImage($path, $msgId, "Billedtekst mangler.");
+		}		
+		echo "success"; // Magic return string to the AJAX handler.
+	}
+}
---
+++ tex/implementation/controller_example_main.php
@@ -0,0 +1,44 @@
+class Main extends GirafController
+{
+	public function index()
+	{
+		// The default always defers to fallback.
+		$params = array("controller" => "Main", "action" => "overview");
+		$this->fallback($params["action"], $params);
+	}
+	
+	/**
+	 * Main slightly mis-uses the intentions of the fallback function.
+	 * It uses the fallback to identity single modules requested in an
+	 * out-of-order way and load them as any other requested module.
+	 * */
+	public function fallback($action, $params = array())
+	{
+		// Prepare data array.
+		$data = array();
+		
+		// Since it's a feature used numerous times throughout this script,
+		// let's just get the current user.
+
+		$s = GirafSession::getSession();
+
+		$currentUserData = GirafUser::getGirafUser($s->getCurrentUser());
+
+		// In order to make the data more digestible to the view, we'll
+		// order the necessary data in the following nesting:
+		// Groups -> Children -> Apps. Although apps and children are
+		// currently just mass-read into arrays, the implementation may
+		// make AJAX/JSON loading later more easily supported. So
+		// without further ado:
+		
+		$data["userId"] = $currentUserData->id;
+		
+		// Invoke the views.
+		$this->view("default/header", $data);
+		$this->view("default/main_stub", $data);
+		// The view should be capable of calling the required sub modules.
+		// We need a standardisation in javascript that makes sure it
+		// knows how to replace the module section. No php there.
+		$this->view("default/footer", $data);
+	}
+}
